5c884d58b9b69e8e112a5d9cf104957571ae488a
metarya07 <metaryajain07@gmail.com>
2026-09-02 11:58:03 +0530

    Update Codeforces 4A Watermelon with full Java source code

--- problems/0004A-watermelon/solution_1.java
+++ problems/0004A-watermelon/solution_1.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Codeforces Problem 4A: Watermelon
  * Problem URL: https://codeforces.com/contest/4/problem/A
  * Submission URL: https://codeforces.com/contest/4/submission/389162586
@@ -10,4 +10,18 @@
  * Author: MetaryaJain
  */
 
-// Solution code submitted on Codeforces
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int w = sc.nextInt();
+            if (w > 2 && w % 2 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
