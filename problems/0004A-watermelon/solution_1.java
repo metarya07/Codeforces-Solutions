9f791b96954e6bd4044d810e83712ffdaa1aadcf
metarya07 <metaryajain07@gmail.com>
2026-09-02 06:21:16 +0000

    [Codeforces 4A] Watermelon - Solution 1 (Java 21)

---
+++ problems/0004A-watermelon/solution_1.java
@@ -0,0 +1,13 @@
+/*
+ * Codeforces Problem 4A: Watermelon
+ * Problem URL: https://codeforces.com/contest/4/problem/A
+ * Submission URL: https://codeforces.com/contest/4/submission/389162586
+ * Solution #1 (Java 21)
+ * Verdict: OK
+ * Time: 500 ms
+ * Memory: 1900 KB
+ * Submitted At: 2026-09-02 06:21:16 UTC
+ * Author: MetaryaJain
+ */
+
+// Solution code submitted on Codeforces
