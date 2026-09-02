6c4f0bd1dfafad49cf0eae406286a4c9be2bc7dd
metarya <metaryajain07@gmail.com>
2026-09-02 12:39:27 +0530

    [Codeforces 71A] Way Too Long Words - Solution 5 (Java 21)

--- problems/0071A-way-too-long-words/solution_5.java
+++ problems/0071A-way-too-long-words/solution_5.java
@@ -1,12 +1,12 @@
 /*
  * Codeforces Problem 71A: Way Too Long Words
  * Problem URL: https://codeforces.com/contest/71/problem/A
- * Submission URL: https://codeforces.com/contest/71/submission/389164619
+ * Submission URL: https://codeforces.com/contest/71/submission/389164092
  * Solution #5 (Java 21)
  * Verdict: Accepted
- * Time: 203 ms
- * Memory: 1100 KB
- * Submission Date: 2026-09-02 07:09:24 UTC
+ * Time: 171 ms
+ * Memory: 600 KB
+ * Submission Date: 2026-09-02 07:09:27 UTC
  * Author: MetaryaJain
  */
 
@@ -14,11 +14,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        new BufferedReader(new InputStreamReader(System.in))
-            .lines()
-            .skip(1) // Skip the first line containing 'n'
-            .map(s -> s.length() > 10 ? s.charAt(0) + "" + (s.length() - 2) + s.charAt(s.length() - 1) : s)
-            .forEach(System.out::println);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine().trim());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine().trim();
+            int len = word.length();
+            
+            if (len > 10) {
+                sb.append(word.charAt(0))
+                  .append(len - 2)
+                  .append(word.charAt(len - 1))
+                  .append("\n");
+            } else {
+                sb.append(word).append("\n");
+            }
+        }
+        System.out.print(sb);
     }
 }
