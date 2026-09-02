b4a3e398340d3eed8c4d8f5f88bcabce2323e9b3
metarya <metaryajain07@gmail.com>
2026-09-02 12:40:57 +0530

    [Codeforces 71A] Way Too Long Words - Solution 6 (Java 21)

--- problems/0071A-way-too-long-words/solution_6.java
+++ problems/0071A-way-too-long-words/solution_6.java
@@ -1,32 +1,24 @@
 /*
  * Codeforces Problem 71A: Way Too Long Words
  * Problem URL: https://codeforces.com/contest/71/problem/A
- * Submission URL: https://codeforces.com/contest/71/submission/389164955
+ * Submission URL: https://codeforces.com/contest/71/submission/389164619
  * Solution #6 (Java 21)
  * Verdict: Accepted
- * Time: 281 ms
- * Memory: 1000 KB
- * Submission Date: 2026-09-02 07:10:56 UTC
+ * Time: 203 ms
+ * Memory: 1100 KB
+ * Submission Date: 2026-09-02 07:10:58 UTC
  * Author: MetaryaJain
  */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    // Helper method to process a single word
-    private static String abbreviate(String word) {
-        if (word.length() <= 10) {
-            return word;
-        }
-        return word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            System.out.println(abbreviate(sc.next()));
-        }
+        new BufferedReader(new InputStreamReader(System.in))
+            .lines()
+            .skip(1) // Skip the first line containing 'n'
+            .map(s -> s.length() > 10 ? s.charAt(0) + "" + (s.length() - 2) + s.charAt(s.length() - 1) : s)
+            .forEach(System.out::println);
     }
 }
