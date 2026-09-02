e56e8ec86b6c615c7d3609ce6e4987f5ecd55fc7
metarya <metaryajain07@gmail.com>
2026-09-02 12:40:31 +0530

    [Codeforces 71A] Way Too Long Words - Solution 6 (Java 21)

---
+++ problems/0071A-way-too-long-words/solution_6.java
@@ -0,0 +1,32 @@
+/*
+ * Codeforces Problem 71A: Way Too Long Words
+ * Problem URL: https://codeforces.com/contest/71/problem/A
+ * Submission URL: https://codeforces.com/contest/71/submission/389164955
+ * Solution #6 (Java 21)
+ * Verdict: Accepted
+ * Time: 281 ms
+ * Memory: 1000 KB
+ * Submission Date: 2026-09-02 07:10:32 UTC
+ * Author: MetaryaJain
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    // Helper method to process a single word
+    private static String abbreviate(String word) {
+        if (word.length() <= 10) {
+            return word;
+        }
+        return word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(abbreviate(sc.next()));
+        }
+    }
+}
