a8b1320ae96323ec16fc1b5ffbbb6fec54490f08
metarya <metaryajain07@gmail.com>
2026-09-02 13:47:47 +0530

    [Codeforces 71A] Way Too Long Words - Solution 11 (Java 21)

---
+++ problems/0071A-way-too-long-words/solution_11.java
@@ -0,0 +1,24 @@
+/*
+ * Codeforces Problem 71A: Way Too Long Words
+ * Problem URL: https://codeforces.com/contest/71/problem/A
+ * Submission URL: https://codeforces.com/contest/71/submission/389164619
+ * Solution #11 (Java 21)
+ * Verdict: Accepted
+ * Time: 203 ms
+ * Memory: 1100 KB
+ * Submission Date: 2026-09-02 08:17:47 UTC
+ * Author: MetaryaJain
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        new BufferedReader(new InputStreamReader(System.in))
+            .lines()
+            .skip(1) // Skip the first line containing 'n'
+            .map(s -> s.length() > 10 ? s.charAt(0) + "" + (s.length() - 2) + s.charAt(s.length() - 1) : s)
+            .forEach(System.out::println);
+    }
+}
